d84f4f14f7e49223bad40640fb7a0a90cd50dc83
Tanmay Dutt <tanmaydutt97@gmail.com>
2019-07-04 16:35:06 +0530

    Update twitter.php

--- Twitter/twitter.php
+++ Twitter/twitter.php
@@ -1,10 +1,10 @@
 <?php
 require_once('TwitterAPIExchange.php');
 $settings = array(
-'oauth_access_token' => "1366594261-h3HjV92RfJ8P5FSG7M2FofZ1k5KhbbuaaF5J9yV",
-'oauth_access_token_secret' => "Yhq9IWb1K6BeVQStojeHBNSvif5TE66jXwgYymtRSQkCA",
-'consumer_key' => "euwC4L7xOsWIXmntV2BVkhnHJ",
-'consumer_secret' => "alZpjA5kdOs9eaNvmqsKqtFSeASvAh9gnFHQBg7y6Vy0RFEuOp"
+'oauth_access_token' => "",
+'oauth_access_token_secret' => "",
+'consumer_key' => "=",
+'consumer_secret' => ""
 );
 $url = "https://api.twitter.com/1.1/search/tweets.json";
 $requestMethod = "GET";
